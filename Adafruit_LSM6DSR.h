1e60c7bb45f2ef473128385fe66fd3d4e4b329b1
JSE-Sendance <114486666+JOSE-Sendance@users.noreply.github.com>
2023-02-01 20:24:28 +0100

    First test converting float to stringstream

--- Adafruit_LSM6DSR.h
+++ Adafruit_LSM6DSR.h
@@ -20,6 +20,10 @@
 
 #include "Adafruit_LSM6DS.h"
 
+#include <ostream>
+#include <sstream>
+#include <iomanip>
+
 #define LSM6DSR_CHIP_ID 0x6B ///< LSM6DSL default device id from WHOAMI
 
 #define LSM6DSR_MASTER_CONFIG 0x1A ///< I2C Master config
@@ -37,8 +41,95 @@ public:
   void enableI2CMasterPullups(bool enable_pullups);
   void enablePedometer(bool enable);
 
+  void readAll()
+  {
+    _read();
+
+    std::ostringstream outputString;
+    outputString << std::fixed;
+    outputString << std::setprecision(4); // maybe set this precision to according resolution of sensor
+
+    outputString << "[" << millis() << ";";
+    outputString << accX << "," << accY << "," << accZ << ",";
+    outputString << std::setprecision(3); // gyro values have less precision, because of different conversion
+    outputString << gyroX << "," << gyroY << "," << gyroZ << "]"; 
+
+    Serial.println(outputString.str().c_str());
+  }
+
 private:
-  bool _init(int32_t sensor_id) override;
+  bool
+  _init(int32_t sensor_id) override;
+
+  // this is the same as in base class, just the conversion differs (acc to mg, gyro to mdeg)
+  void _read(void) override
+  {
+    // get raw readings
+    Adafruit_BusIO_Register data_reg = Adafruit_BusIO_Register(
+        i2c_dev, spi_dev, ADDRBIT8_HIGH_TOREAD, LSM6DS_OUT_TEMP_L, 14);
+
+    uint8_t buffer[14];
+    data_reg.read(buffer, 14);
+
+    rawTemp = buffer[1] << 8 | buffer[0];
+    temperature = (rawTemp / temperature_sensitivity) + 25.0;
+
+    rawGyroX = buffer[3] << 8 | buffer[2];
+    rawGyroY = buffer[5] << 8 | buffer[4];
+    rawGyroZ = buffer[7] << 8 | buffer[6];
+
+    rawAccX = buffer[9] << 8 | buffer[8];
+    rawAccY = buffer[11] << 8 | buffer[10];
+    rawAccZ = buffer[13] << 8 | buffer[12];
+
+    float gyro_scale = 1; // range is in milli-dps per bit!
+    switch (gyroRangeBuffered)
+    {
+    case ISM330DHCX_GYRO_RANGE_4000_DPS:
+      gyro_scale = 140.0;
+      break;
+    case LSM6DS_GYRO_RANGE_2000_DPS:
+      gyro_scale = 70.0;
+      break;
+    case LSM6DS_GYRO_RANGE_1000_DPS:
+      gyro_scale = 35.0;
+      break;
+    case LSM6DS_GYRO_RANGE_500_DPS:
+      gyro_scale = 17.50;
+      break;
+    case LSM6DS_GYRO_RANGE_250_DPS:
+      gyro_scale = 8.75;
+      break;
+    case LSM6DS_GYRO_RANGE_125_DPS:
+      gyro_scale = 4.375;
+      break;
+    }
+
+    gyroX = rawGyroX * gyro_scale;
+    gyroY = rawGyroY * gyro_scale;
+    gyroZ = rawGyroZ * gyro_scale;
+
+    float accel_scale = 1; // range is in milli-g per bit!
+    switch (accelRangeBuffered)
+    {
+    case LSM6DS_ACCEL_RANGE_16_G:
+      accel_scale = 0.488;
+      break;
+    case LSM6DS_ACCEL_RANGE_8_G:
+      accel_scale = 0.244;
+      break;
+    case LSM6DS_ACCEL_RANGE_4_G:
+      accel_scale = 0.122;
+      break;
+    case LSM6DS_ACCEL_RANGE_2_G:
+      accel_scale = 0.061;
+      break;
+    }
+
+    accX = rawAccX * accel_scale;
+    accY = rawAccY * accel_scale;
+    accZ = rawAccZ * accel_scale;
+  }
 };
 
 #endif
